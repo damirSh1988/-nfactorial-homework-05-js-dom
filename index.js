7a98af051004b476dfb584e5246d202b1eb9f688
damirSh1988 <shayakhmet_d@mail.ru>
2023-01-22 02:56:07 +0600

    add file

---
+++ index.js
@@ -0,0 +1,13 @@
+const tadleId = document.getElementById('age-table');
+console.log(tadleId);
+
+const ageLabel = document.querySelectorAll('#age-table label');
+console.log(ageLabel);
+
+const tdLabel = document.querySelector('#age-table td');
+console.log(tdLabel);
+
+const formName = document.getElementsByName('search')
+const firstInput = formName.querySelectorAll('input')[0];
+console.log(formName);
+console.log(firstInput);
